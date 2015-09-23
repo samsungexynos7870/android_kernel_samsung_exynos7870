999b1b568e8d6a76ebaef8740103fac60ea70f92
Bendik Rønning Opstad <bro.devel@gmail.com>
2015-09-23 18:49:53 +0200

    tcp: Fix CWV being too strict on thin streams
    Application limited streams such as thin streams, that transmit small
    amounts of payload in relatively few packets per RTT, can be prevented
    from growing the CWND when in congestion avoidance. This leads to
    increased sojourn times for data segments in streams that often transmit
    time-dependent data.
    
    Currently, a connection is considered CWND limited only after having
    successfully transmitted at least one packet with new data, while at the
    same time failing to transmit some unsent data from the output queue
    because the CWND is full. Applications that produce small amounts of
    data may be left in a state where it is never considered to be CWND
    limited, because all unsent data is successfully transmitted each time
    an incoming ACK opens up for more data to be transmitted in the send
    window.
    
    Fix by always testing whether the CWND is fully used after successful
    packet transmissions, such that a connection is considered CWND limited
    whenever the CWND has been filled. This is the correct behavior as
    specified in RFC2861 (section 3.1).
    
    Cc: Andreas Petlund <apetlund@simula.no>
    Cc: Carsten Griwodz <griff@simula.no>
    Cc: Jonas Markussen <jonassm@ifi.uio.no>
    Cc: Kenneth Klette Jonassen <kennetkl@ifi.uio.no>
    Cc: Mads Johannessen <madsjoh@ifi.uio.no>
    Signed-off-by: Bendik Rønning Opstad <bro.devel+kernel@gmail.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Tested-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Tested-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1812,7 +1812,7 @@ static bool tcp_tso_should_defer(struct sock *sk, struct sk_buff *skb,
 	if (!tp->tso_deferred)
 		tp->tso_deferred = 1 | (jiffies << 1);
 
-	if (cong_win < send_win && cong_win < skb->len)
+	if (cong_win < send_win && cong_win <= skb->len)
 		*is_cwnd_limited = true;
 
 	return true;
@@ -2007,7 +2007,6 @@ static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 
 		cwnd_quota = tcp_cwnd_test(tp, skb);
 		if (!cwnd_quota) {
-			is_cwnd_limited = true;
 			if (push_one == 2)
 				/* Force out a loss probe pkt. */
 				cwnd_quota = 1;
@@ -2096,6 +2095,7 @@ repair:
 		/* Send one loss probe per tail loss episode. */
 		if (push_one != 2)
 			tcp_schedule_loss_probe(sk);
+		is_cwnd_limited |= (tcp_packets_in_flight(tp) >= tp->snd_cwnd);
 		tcp_cwnd_validate(sk, is_cwnd_limited);
 		return false;
 	}
