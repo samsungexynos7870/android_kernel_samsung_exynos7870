a4e44991776ec47c5218e5f7bcdb9a8727a71b77
Ricard Wanderlof <ricard.wanderlof@axis.com>
2015-10-19 08:52:50 +0200

    ALSA: USB-audio: Also move out hwptr_done wrap from prepare_playback_urb()
    Refactoring in preparation for adding Zoom R16/24 quirk.
    No functional change.
    
    Signed-off-by: Ricard Wanderlof <ricardw@axis.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/pcm.c
+++ sound/usb/pcm.c
@@ -1367,6 +1367,8 @@ static inline void fill_playback_urb_dsd_dop(struct snd_usb_substream *subs,
 			subs->hwptr_done++;
 		}
 	}
+	if (subs->hwptr_done >= runtime->buffer_size * stride)
+		subs->hwptr_done -= runtime->buffer_size * stride;
 }
 
 static void copy_to_urb(struct snd_usb_substream *subs,
@@ -1387,6 +1389,8 @@ static void copy_to_urb(struct snd_usb_substream *subs,
 		       runtime->dma_area + subs->hwptr_done, bytes);
 	}
 	subs->hwptr_done += bytes;
+	if (subs->hwptr_done >= runtime->buffer_size * stride)
+		subs->hwptr_done -= runtime->buffer_size * stride;
 }
 
 static void prepare_playback_urb(struct snd_usb_substream *subs,
@@ -1464,13 +1468,12 @@ static void prepare_playback_urb(struct snd_usb_substream *subs,
 		}
 
 		subs->hwptr_done += bytes;
+		if (subs->hwptr_done >= runtime->buffer_size * stride)
+			subs->hwptr_done -= runtime->buffer_size * stride;
 	} else {
 		/* usual PCM */
 		copy_to_urb(subs, urb, stride, bytes);
 	}
-
-	if (subs->hwptr_done >= runtime->buffer_size * stride)
-		subs->hwptr_done -= runtime->buffer_size * stride;
 
 	/* update delay with exact number of samples queued */
 	runtime->delay = subs->last_delay;
