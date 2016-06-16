16eafdc05015c37c34d19ef516efbb8d71b55cb0
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2016-06-16 08:27:36 +0200

    serial: samsung: Fix possible out of bounds access on non-DT platform
    commit 926b7b5122c96e1f18cd20e85a286c7ec8d18c97 upstream.
    
    On non-DeviceTree platforms, the index of serial device is a static
    variable incremented on each probe.  It is incremented even if deferred
    probe happens when getting the clock in s3c24xx_serial_init_port().
    
    This index is used for referencing elements of statically allocated
    s3c24xx_serial_ports array.  In case of re-probe, the index will point
    outside of this array leading to memory corruption.
    
    Increment the index only on successful probe.
    
    Reported-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Fixes: b497549a035c ("[ARM] S3C24XX: Split serial driver into core and per-cpu drivers")
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -1866,8 +1866,6 @@ static int s3c24xx_serial_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
-	probe_index++;
-
 	dbg("%s: initialising port %p...\n", __func__, ourport);
 
 #ifdef CONFIG_ARM_EXYNOS_DEVFREQ
@@ -2016,6 +2014,8 @@ static int s3c24xx_serial_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "failed to create sysfs file.\n");
 
 	ourport->dbg_mode = 0;
+
+	probe_index++;
 
 	return 0;
 }
