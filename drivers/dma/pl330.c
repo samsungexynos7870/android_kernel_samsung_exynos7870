02cfd7796aa6f28a9186ebdd205dc42438229901
Iago Abal <mail@iagoabal.eu>
2017-01-11 14:00:21 +0100

    dmaengine: pl330: fix double lock
    [ Upstream commit 91539eb1fda2d530d3b268eef542c5414e54bf1a ]
    
    The static bug finder EBA (http://www.iagoabal.eu/eba/) reported the
    following double-lock bug:
    
        Double lock:
        1. spin_lock_irqsave(pch->lock, flags) at pl330_free_chan_resources:2236;
        2. call to function `pl330_release_channel' immediately after;
        3. call to function `dma_pl330_rqcb' in line 1753;
        4. spin_lock_irqsave(pch->lock, flags) at dma_pl330_rqcb:1505.
    
    I have fixed it as suggested by Marek Szyprowski.
    
    First, I have replaced `pch->lock' with `pl330->lock' in functions
    `pl330_alloc_chan_resources' and `pl330_free_chan_resources'. This avoids
    the double-lock by acquiring a different lock than `dma_pl330_rqcb'.
    
    NOTE that, as a result, `pl330_free_chan_resources' executes
    `list_splice_tail_init' on `pch->work_list' under lock `pl330->lock',
    whereas in the rest of the code `pch->work_list' is protected by
    `pch->lock'. I don't know if this may cause race conditions. Similarly
    `pch->cyclic' is written by `pl330_alloc_chan_resources' under
    `pl330->lock' but read by `pl330_tx_submit' under `pch->lock'.
    
    Second, I have removed locking from `pl330_request_channel' and
    `pl330_release_channel' functions. Function `pl330_request_channel' is
    only called from `pl330_alloc_chan_resources', so the lock is already
    held. Function `pl330_release_channel' is called from
    `pl330_free_chan_resources', which already holds the lock, and from
    `pl330_del'. Function `pl330_del' is called in an error path of
    `pl330_probe' and at the end of `pl330_remove', but I assume that there
    cannot be concurrent accesses to the protected data at those points.
    
    Signed-off-by: Iago Abal <mail@iagoabal.eu>
    Reviewed-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ibbc4829ad46b281893fe2a7a2d255d2d3df157be

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -1795,15 +1795,12 @@ static bool _chan_ns(const struct pl330_dmac *pl330, int i)
 static struct pl330_thread *pl330_request_channel(struct pl330_dmac *pl330)
 {
 	struct pl330_thread *thrd = NULL;
-	unsigned long flags;
 	int chans, i;
 
 	if (pl330->state == DYING)
 		return NULL;
 
 	chans = pl330->pcfg.num_chan;
-
-	spin_lock_irqsave(&pl330->lock, flags);
 
 	for (i = 0; i < chans; i++) {
 		thrd = &pl330->channels[i];
@@ -1822,8 +1819,6 @@ static struct pl330_thread *pl330_request_channel(struct pl330_dmac *pl330)
 		}
 		thrd = NULL;
 	}
-
-	spin_unlock_irqrestore(&pl330->lock, flags);
 
 	return thrd;
 }
@@ -1844,7 +1839,6 @@ static inline void _free_event(struct pl330_thread *thrd, int ev)
 static void pl330_release_channel(struct pl330_thread *thrd)
 {
 	struct pl330_dmac *pl330;
-	unsigned long flags;
 
 	if (!thrd || thrd->free)
 		return;
@@ -1856,10 +1850,8 @@ static void pl330_release_channel(struct pl330_thread *thrd)
 
 	pl330 = thrd->dmac;
 
-	spin_lock_irqsave(&pl330->lock, flags);
 	_free_event(thrd, thrd->ev);
 	thrd->free = true;
-	spin_unlock_irqrestore(&pl330->lock, flags);
 }
 
 /* Initialize the structure for PL330 configuration, that can be used
@@ -2236,20 +2228,20 @@ static int pl330_alloc_chan_resources(struct dma_chan *chan)
 	pm_runtime_get_sync(pl330->ddma.dev);
 #endif
 
-	spin_lock_irqsave(&pch->lock, flags);
+	spin_lock_irqsave(&pl330->lock, flags);
 
 	dma_cookie_init(chan);
 	pch->cyclic = false;
 
 	pch->thread = pl330_request_channel(pl330);
 	if (!pch->thread) {
-		spin_unlock_irqrestore(&pch->lock, flags);
+		spin_unlock_irqrestore(&pl330->lock, flags);
 		return -ENOMEM;
 	}
 
 	tasklet_init(&pch->task, pl330_tasklet, (unsigned long) pch);
 
-	spin_unlock_irqrestore(&pch->lock, flags);
+	spin_unlock_irqrestore(&pl330->lock, flags);
 
 	return 1;
 }
@@ -2326,11 +2318,12 @@ static int pl330_control(struct dma_chan *chan, enum dma_ctrl_cmd cmd, unsigned 
 static void pl330_free_chan_resources(struct dma_chan *chan)
 {
 	struct dma_pl330_chan *pch = to_pchan(chan);
+	struct pl330_dmac *pl330 = pch->dmac;
 	unsigned long flags;
 
 	tasklet_kill(&pch->task);
 
-	spin_lock_irqsave(&pch->lock, flags);
+	spin_lock_irqsave(&pl330->lock, flags);
 
 	pl330_release_channel(pch->thread);
 	pch->thread = NULL;
@@ -2338,7 +2331,7 @@ static void pl330_free_chan_resources(struct dma_chan *chan)
 	if (pch->cyclic)
 		list_splice_tail_init(&pch->work_list, &pch->dmac->desc_pool);
 
-	spin_unlock_irqrestore(&pch->lock, flags);
+	spin_unlock_irqrestore(&pl330->lock, flags);
 
 #ifdef CONFIG_PM_RUNTIME
 	pm_runtime_put_sync(pch->dmac->ddma.dev);
