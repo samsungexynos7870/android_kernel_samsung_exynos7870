dd3395aa012194bb2e43971b6c5d1d17199670d9
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:41:37 -0700

    qcacld-2.0: Fix misleading indentation warning in limEnableHTNonGfProtection
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limUtils.c
+++ drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limUtils.c
@@ -4345,8 +4345,8 @@ limEnableHTLsigTxopProtection(tpAniSirGlobal pMac, tANI_U8 enable,
     if(!psessionEntry->htCapability)
         return eSIR_SUCCESS; // this protection  is only for HT stations.
 
-        //overlapping protection configuration check.
-        if(overlap) {
+    //overlapping protection configuration check.
+    if(overlap) {
         } else {
             //normal protection config check
             if (LIM_IS_AP_ROLE(psessionEntry) &&
