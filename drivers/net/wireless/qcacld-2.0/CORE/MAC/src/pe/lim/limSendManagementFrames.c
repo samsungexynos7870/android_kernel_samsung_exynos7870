c15f83fa80f493bf0c8d30dd514025ab414eb813
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:37:32 -0700

    qcacld-2.0: Fix misleading indentation warning in limSendDelBAInd
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
+++ drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -5499,7 +5499,7 @@ tSirRetStatus limSendDelBAInd( tpAniSirGlobal pMac,
     else
       return eSIR_SUCCESS;
 
-      returnAfterError:
+returnAfterError:
 
       // Release buffer, if allocated
       if( NULL != pDelBAIndBuffer )
