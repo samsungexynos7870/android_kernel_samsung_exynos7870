9ce3b965395b03e1ac283e7d06dc22c354037458
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:36:51 -0700

    qcacld-2.0: Fix misleading indentation warning in limSendAddBARsp
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
+++ drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -5316,7 +5316,7 @@ tSirRetStatus limSendAddBARsp( tpAniSirGlobal pMac,
    else
       return eSIR_SUCCESS;
 
-      returnAfterError:
+returnAfterError:
 
       // Release buffer, if allocated
       if( NULL != pAddBARspBuffer )
