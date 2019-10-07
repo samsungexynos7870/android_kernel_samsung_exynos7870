73e76a4066f450e72fb276f208afcb2d39f4958b
Astrako <apamarin@gmail.com>
2019-10-07 19:40:35 +0200

    update build_kernel.sh script

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,5 @@ export ARCH=arm64
 export CROSS_COMPILE=~/android/kernel/toolchain/gcc-linaro-7.4.1/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
-make O=./out a3y17ltexc_00_defconfig
+make O=./out exynos7870-a3y17lte_defconfig
 make O=./out -j16
