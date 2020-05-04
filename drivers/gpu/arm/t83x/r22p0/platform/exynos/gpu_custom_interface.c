ce5f1d8a7151667c6c674952746ce13ab5fd4af9
TALU <taluemail272@gmail.com>
2020-05-04 06:47:07 +0100

    gpu: arm: t83x: r22p0: custom_interface: decrease the minimum voltage to 0.25V.

--- drivers/gpu/arm/t83x/r22p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/t83x/r22p0/platform/exynos/gpu_custom_interface.c
@@ -35,7 +35,7 @@
 
 #ifdef CONFIG_SOC_EXYNOS7870
 #define GPU_MAX_VOLT		1100000
-#define GPU_MIN_VOLT		500000
+#define GPU_MIN_VOLT		250000
 #define GPU_VOLT_STEP		1
 #else
 #error "Please define gpu voltage ranges for current SoC."
