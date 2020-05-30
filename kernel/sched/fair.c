bbf7f99f1b74864220473be10efb191fa1ec05ca
TALU <taluemail272@gmail.com>
2020-05-30 11:12:27 +0100

    sched/fair: change HMP up threshold to 512.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2708,7 +2708,7 @@ static inline void update_rq_runnable_avg(struct rq *rq, int runnable) {}
  * tweaking suit particular needs.
  */
 
-unsigned int hmp_up_threshold = 700;
+unsigned int hmp_up_threshold = 512;
 unsigned int hmp_down_threshold = 256;
 
 unsigned int hmp_semiboost_up_threshold = 400;
