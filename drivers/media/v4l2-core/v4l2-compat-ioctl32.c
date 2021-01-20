e7310edc34fcdc69aa671f826cf0978fed07c844
TALU <taluemail272@gmail.com>
2021-01-20 19:33:24 +0000

    v4l2-core: compat-ioctl32: import two changes from M105GDXU5CTI3.

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -487,7 +487,8 @@ static int get_v4l2_buffer32(struct v4l2_buffer __user *kp,
 	    get_user(memory, &up->memory) ||
 	    put_user(memory, &kp->memory) ||
 	    get_user(length, &up->length) ||
-	    put_user(length, &kp->length))
+	    put_user(length, &kp->length) ||
+		assign_in_user(&kp->reserved, &up->reserved))
 		return -EFAULT;
 
 	if (V4L2_TYPE_IS_OUTPUT(type))
@@ -496,7 +497,10 @@ static int get_v4l2_buffer32(struct v4l2_buffer __user *kp,
 		    assign_in_user(&kp->timestamp.tv_sec,
 				   &up->timestamp.tv_sec) ||
 		    assign_in_user(&kp->timestamp.tv_usec,
-				   &up->timestamp.tv_usec))
+				   &up->timestamp.tv_usec)||
+			copy_in_user(&kp->timecode, &up->timecode, sizeof(struct v4l2_timecode)) ||
+			assign_in_user(&kp->sequence, &up->sequence) ||
+			assign_in_user(&kp->reserved2, &up->reserved2))
 			return -EFAULT;
 
 	if (V4L2_TYPE_IS_MULTIPLANAR(type)) {
